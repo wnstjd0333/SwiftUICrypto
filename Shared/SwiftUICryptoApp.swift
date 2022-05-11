9a1863184a4454ebf5a9ab4b33adabd246bb2e1c
kimjunseong <kimjunseong@archive-asia.co.jp>
2022-05-11 12:55:49 +0900

    create project

---
+++ Shared/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  Shared
+//
+//  Created by 金峻聖 on 2022/05/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
