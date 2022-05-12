312907bffaf802017b88fbb895a25e1b701ccd54
kimjunseong <kimjunseong@archive-asia.co.jp>
2022-05-13 00:13:28 +0900

    add: statistic model, view

---
+++ Shared/Core/Components/StatisticView.swift
@@ -0,0 +1,50 @@
+//
+//  StatisticView.swift
+//  SwiftUICrypto (iOS)
+//
+//  Created by 金峻聖 on 2022/05/12.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    
+    let stat: StatisticModel
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(stat.title)
+                .font(.caption)
+                .foregroundColor(.theme.secondaryText)
+            Text(stat.value)
+                .font(.headline)
+                .foregroundColor(.theme.accent)
+            HStack(spacing: 4) {
+                Image(systemName: "triangle.fill")
+                    .font(.caption2)
+                    .rotationEffect(
+                        Angle(degrees: (stat.percentageChange ?? 0) >= 0 ? 0 : 180))
+                Text(stat.percentageChange?.asPerscentString() ?? "")
+                    .font(.caption)
+                    .bold()
+            }
+            .foregroundColor((stat.percentageChange ?? 0) >= 0 ? .theme.green : .theme.red)
+            .opacity(stat.percentageChange == nil ? 0.0 : 1.0)
+        }
+    }
+}
+
+struct StatisticView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            StatisticView(stat: dev.stat1)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+            StatisticView(stat: dev.stat2)
+                .previewLayout(.sizeThatFits)
+            StatisticView(stat: dev.stat3)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
