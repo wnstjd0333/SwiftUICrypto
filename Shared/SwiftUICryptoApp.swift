1811fc6da32853adec3fa874b9bd2a05075069ba
kimjunseong <kimjunseong@archive-asia.co.jp>
2022-05-12 20:03:03 +0900

    add: dynamic list to display downloaded coins

--- Shared/SwiftUICryptoApp.swift
+++ Shared/SwiftUICryptoApp.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 @main
 struct SwiftUICryptoApp: App {
+    
+    @StateObject var vm = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 HomeView()
                     .navigationBarHidden(true)
             }
+            .environmentObject(vm)
         }
     }
 }
