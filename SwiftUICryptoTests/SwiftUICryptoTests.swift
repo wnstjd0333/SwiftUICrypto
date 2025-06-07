05a8190c1ff11aaf9ee7831096923769c22be6a4
archive_kimjs <kimjunseong@archive-asia.co.jp>
2025-06-07 20:51:18 +0900

    add test module

---
+++ SwiftUICryptoTests/SwiftUICryptoTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUICryptoTests.swift
+//  SwiftUICryptoTests
+//
+//  Created by Junseong Kim on 2025/06/07.
+//
+
+import Testing
+
+struct SwiftUICryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
